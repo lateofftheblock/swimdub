f4e97431222e5f598391c0db57b3843e757508dd
lateofftheblock <bodonnel@mail.umw.edu>
2012-10-18 12:39:48 -0400

    woops

---
+++ addUser.php
@@ -0,0 +1,27 @@
+<?php
+include("db_connect.php");
+$first=$_GET['first'];
+$last=$_GET['last'];
+$email=$_GET['email'];
+$admin=$_GET['admin'];
+$pwd= "eagles";
+if($first != null && $last != null && $email != null){
+	$query = "SELECT * FROM users WHERE email = '$email'";
+	$result = mysqli_query($db, $query);
+	
+	if ($row = mysqli_fetch_array($result)){
+		include("modifyUsers.php");
+		echo"this user already exists!";
+	}else{
+
+        $addUser= "INSERT INTO users (first_name, last_name, email, pwd, admin) VALUES('$first', '$last', '$email', '$pwd', '$admin')";
+	$adding= mysqli_query($db, $addUser);
+	header("Location: modifyUsers.php");
+
+	}
+}else{
+	include("modifyUsers.php");
+	echo "Please complete the entire form";
+}
+
+?>
